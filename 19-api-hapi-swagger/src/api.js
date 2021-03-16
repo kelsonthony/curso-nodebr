d802037c34add01f9be6aef0f5cc5129563e43f0
kelsonthony <k.thony@gmail.com>
2021-03-15 22:01:03 -0300

    Update Modulo 10

---
+++ 19-api-hapi-swagger/src/api.js
@@ -0,0 +1,51 @@
+const Hapi = require('hapi')
+const Context = require('./db/strategies/base/contextStrategy')
+const MongoDb = require('./db/strategies/mongodb/mongodb')
+const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema')
+const HeroRoute = require('./routes/heroRoutes')
+const HapiSwagger = require('hapi-swagger')
+const Vision = require('vision')
+const Inert = require('inert')
+
+const app = new Hapi.Server({
+    port: 4000
+}) 
+
+function mapRoutes(instance, methods) {
+    
+    return methods.map(method => instance[method]())
+}
+
+async function main() {
+
+    const connection = MongoDb.connect()
+    const context = new Context(new MongoDb(connection, HeroiSchema))
+
+    const swaggerOptions = {
+        info: {
+            title: 'API Herois',
+            version: 'v1.0'
+        },
+        lang: 'pt'
+    }
+
+    await app.register([
+        Vision,
+        Inert,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ])
+
+    app.route(
+        mapRoutes(new HeroRoute(context), HeroRoute.methods())
+    )
+
+    await app.start()
+    console.log('Server rodando na porta', app.info.port)
+
+    return app
+
+}
+module.exports = main()
